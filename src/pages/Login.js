bed22a99888ec77e496d89ddb6f5a3be4bf149a6
Thinh Nguyen <thinh-nguyen@hotmail.com>
2020-10-06 22:04:39 -0400

    added login page

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { signin } from "../helpers/auth";
 
-const Login = () => {
-  return <h1>Login</h1>;
+const SignUp = () => {
+  const [error, setError] = React.useState(null);
+  const [email, setEmail] = React.useState("");
+  const [password, setPassword] = React.useState("");
+
+  function handleEmailChange(event) {
+    setEmail(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setError("");
+    try {
+      await signin(email, password);
+    } catch (error) {
+      setError(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <form autoComplete="off" onSubmit={handleSubmit}>
+        <h1>
+          Login to
+          <Link to="/">Chatty</Link>
+        </h1>
+        <p>Fill in the form below to login to your account.</p>
+        <div>
+          <input
+            placeholder="Email"
+            name="email"
+            type="email"
+            onChange={handleEmailChange}
+            value={email}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Password"
+            name="password"
+            onChange={handlePasswordChange}
+            value={password}
+            type="password"
+          />
+        </div>
+        <div>
+          {error ? <p>{error}</p> : null}
+          <button type="submit">Login</button>
+        </div>
+        <hr />
+        <p>
+          Don't have an account? <Link to="/signup">Sign up</Link>
+        </p>
+      </form>
+    </div>
+  );
 };
 
-export default Login;
+export default SignUp;
